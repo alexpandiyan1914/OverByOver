2eb79649d7f389fe1686c0040650820513e38a1d
Alex potter <alexpandiyan1914@gmail.com>
2025-04-19 23:38:06 +0530

    won msg like professional style

--- js/winner.js
+++ js/winner.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
     let winAtSec = localStorage.getItem("won-sec-innings");
     let winAtFir = localStorage.getItem("won-fir-inning");
+    let firstInng_run = localStorage.getItem("firstInnings_runs");
+    let secondInng_run = localStorage.getItem("runs");
+    let wonbyDiff = firstInng_run - secondInng_run;
+    let wonbyWick = 10-localStorage.getItem("wickets");
 
     if (winAtSec) {
-        document.getElementById("winner").textContent = `${winAtSec} Team Won the Match!`;
+        document.getElementById("winner").textContent = `${winAtSec} won by ${wonbyWick} wkts`;
         console.log(winAtSec,"Won the match !");
     } else if (winAtFir) {
-        document.getElementById("winner").textContent = `${winAtFir} Team Won the Match!`;
+        document.getElementById("winner").textContent = `${winAtFir} won by ${wonbyDiff} runs`;
         console.log(winAtFir,"Won the match !");
     } else {
         document.getElementById("winner").textContent = "Match result not available.";
