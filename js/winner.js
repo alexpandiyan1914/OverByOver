c2a1ddd9fda260f03710311cc75f4d4906c02070
Alex potter <alexpandiyan1914@gmail.com>
2025-04-20 00:18:23 +0530

    match summary is now available in winner.html

--- js/winner.js
+++ js/winner.js
@@ -4,21 +4,46 @@ document.addEventListener("DOMContentLoaded", () => {
     let firstInng_run = localStorage.getItem("firstInnings_runs");
     let secondInng_run = localStorage.getItem("runs");
     let wonbyDiff = firstInng_run - secondInng_run;
-    let wonbyWick = 10-localStorage.getItem("wickets");
+    let wonbyWick = 10 - localStorage.getItem("wickets");
+    let firstInng_over = localStorage.getItem("firstInnings_overs");
+    let sec_over = localStorage.getItem("over");
+    let sec_balls = localStorage.getItem("balls");
+
+    team1 = localStorage.getItem("team1") || "Team A"
+    team2 = localStorage.getItem("team2") || "Team B";
+    tossWinner = localStorage.getItem("tossWinner");
+    tossChoice = localStorage.getItem("tossChoice");
+
+    let firstBattingTeam, secondBattingTeam;
+
+    // Decide who batted first based on toss
+    if (tossChoice === "batting") {
+        firstBattingTeam = tossWinner;
+        secondBattingTeam = (tossWinner === team1) ? team2 : team1;
+    } else {
+        secondBattingTeam = tossWinner;
+        firstBattingTeam = (tossWinner === team1) ? team2 : team1;
+    }
 
     if (winAtSec) {
         document.getElementById("winner").textContent = `${winAtSec} won by ${wonbyWick} wkts`;
-        console.log(winAtSec,"Won the match !");
+        console.log(winAtSec, "Won the match !");
     } else if (winAtFir) {
         document.getElementById("winner").textContent = `${winAtFir} won by ${wonbyDiff} runs`;
-        console.log(winAtFir,"Won the match !");
+        console.log(winAtFir, "Won the match !");
     } else {
         document.getElementById("winner").textContent = "Match result not available.";
         console.log("Match result not available");
     }
+
+
+
+    document.getElementById("team_1").innerText = `${secondBattingTeam} - ${secondInng_run} (${sec_over}.${sec_balls})`;
+    document.getElementById("team_2").innerText = `${firstBattingTeam} - ${firstInng_run} (${firstInng_over})`;
+
 });
 
-function newMatch(){
+function newMatch() {
     localStorage.clear();
     window.location.href = "index.html";
 }
